cb7a0cde19dca47708bda6cdc8a154cbed5c908b
Waesomle <raffiwaesomle@gmail.com>
2015-01-08 16:28:08 -0800

    Added setup for Robot.cpp and Robot.h

---
+++ Freyja/src/Robot.cpp
@@ -0,0 +1,14 @@
+/*
+ * HumanController.cpp
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: Robotics Team 8
+ */
+
+#include <Robot.h>
+
+Robot::Robot() :
+	DriveTrain(),
+	{
+	std::printf("Robot constructor\n")
+	}
