e4c2160e75d3e0bf0db4d3f2a88b780dd65a6573
Raymond Fang <rayfang2000@gmail.com>
2015-01-11 13:56:38 -0800

    changed constants.cpp into constants.h

--- Freyja/src/Constants.h
+++ Freyja/src/Constants.h
@@ -4,8 +4,8 @@
  *  Created on: Jan 7, 2015
  *      Author: EngTechP7
  */
-#ifndef FREYJA_SRC_CONSTANTS_CPP_
-#define FREYJA_SRC_CONSTANTS_CPP_
+#ifndef FREYJA_SRC_CONSTANTS_H_
+#define FREYJA_SRC_CONSTANTS_H_
 #define Joystick
 
 
