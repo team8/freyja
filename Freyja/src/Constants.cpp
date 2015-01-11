f9a8f2319a04e043dd35ffea24a96b41ddb26392
Raymond Fang <rayfang2000@gmail.com>
2015-01-11 13:25:03 -0800

    Defined three ports and Joystick

--- Freyja/src/Constants.cpp
+++ Freyja/src/Constants.cpp
@@ -5,3 +5,15 @@
  *      Author: EngTechP7
  */
 #ifndef FREYJA_SRC_CONSTANTS_CPP_
+#define Joystick
+#ifndef Joystick
+
+
+#endif
+
+
+#if defined Joystick
+#define PORT_SPEED 1
+#define PORT_TURN 2
+#define PORT_OPERATOR 3
+
