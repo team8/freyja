14da1717bf08e69e747a63bd60a668b04d415dbf
Andrew Tam <tam.andrew7@gmail.com>
2015-01-12 15:44:29 -0800

    Update HumanController.cpp

--- Freyja/src/HumanController.cpp
+++ Freyja/src/HumanController.cpp
@@ -15,17 +15,5 @@ HumanController::HumanController() :
 	}
 
 void HumanController::update(Robot *robotPointer) {
-	if (abs(moveJoystick.GetY())<0.1){
-		//set drivetrain forwardspeed to 0
-	}
-	else{
-		//assign getX to drivetrain forwardspeed
-	}
-	if (abs(turnJoystick.GetX())<0.1){
-		//set drivetrain turnspeed to 0
-	}
-	else{
-		//assign getY to drivetrain turnspeed
-	}
 	robotPointer -> move(moveJoystick.GetY(),turnJoystick.GetX());
 }
