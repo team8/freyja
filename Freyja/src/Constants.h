e74ce3d2e48a26055e963712fc927ffe5d0908d1
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-01-12 16:07:03 -0800

    Added fenrir's victor ports

--- Freyja/src/Constants.h
+++ Freyja/src/Constants.h
@@ -1,18 +1,14 @@
-/*
- * Constants.cpp
- *
- *  Created on: Jan 7, 2015
- *      Author: EngTechP7
- */
 #ifndef FREYJA_SRC_CONSTANTS_H_
 #define FREYJA_SRC_CONSTANTS_H_
 #define Joystick
 
+//ports for drive train victors (currently fenrir)
+#define PORT_DRIVE_VIC_LEFT_FRONT 5
+#define PORT_DRIVE_VIC_LEFT_BACK 6
+#define PORT_DRIVE_VIC_RIGHT_FRONT 1
+#define PORT_DRIVE_VIC_RIGHT_BACK 2
 
-
-
-
-#if defined Joystick
+//Joystick ports
 #define PORT_SPEED 1
 #define PORT_TURN 2
 #define PORT_OPERATOR 3
