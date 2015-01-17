f736119b2c70292fcb11368e2b08718ec49171f2
JulianKnodt <julianknodt@gmail.com>
2015-01-17 12:31:55 -0800

    Created Arm.cpp and Added Deconstructor
    From  2015TestPrograms Repository

---
+++ Freyja/src/Arm.cpp
@@ -0,0 +1,49 @@
+#include "Arm.h"
+#include <WPILib.h>
+#include <iostream>
+
+Arm::Arm():
+	compressor(),
+	solenoid((uint32_t) 0, (uint32_t) 1)
+{
+	setState(IDLE);
+}
+
+Arm::~Arm() {
+	// TODO Auto-generated destructor stub
+}
+
+void Arm::setState(State state)
+{
+	this -> state = state;
+}
+
+void Arm::disable()
+{
+	setState(IDLE);
+}
+
+void Arm::update()
+{
+
+	switch(state)
+	{
+		case EXTENDING: 
+			compressor.Start();
+			solenoid.Set(DoubleSolenoid::Value::kForward);
+			break;
+		case RETRACTING:
+			compressor.Start();
+			solenoid.Set(DoubleSolenoid::Value::kReverse);
+			break;
+		case IDLE:
+			compressor.Stop();
+			solenoid.Set(DoubleSolenoid::Value::kOff);
+			break;
+	}
+}
+
+void Arm::init()
+{
+	setState(IDLE);
+}
