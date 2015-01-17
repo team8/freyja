13643af8d713415a3dd5f4d7cf69b5734fc79517
JulianKnodt <julianknodt@gmail.com>
2015-01-17 12:34:31 -0800

    Arm can update etc.

--- Freyja/src/Robot.cpp
+++ Freyja/src/Robot.cpp
@@ -2,20 +2,24 @@
 
 Robot::Robot() :
 	driveTrain()
+	arm()
 {
 	std::cout << "Robot constructor";
 }
 
 void Robot::init() {
 	driveTrain.init();
+	arm.init();
 }
 
 void Robot::update() {
 	driveTrain.update();
+	arm.update();
 }
 
 void Robot::disable() {
 	driveTrain.disable();
+	arm();
 }
 void Robot::move(double Y, double X) {
 	driveTrain.move(Y, X);
