860f7036380c470c73954350094033760d528761
Waesomle <raffiwaesomle@gmail.com>
2015-01-17 16:06:46 -0800

    I have just been altering some sections of the code because there are errors with misplaced parentheses and curly brackets.

--- Freyja/src/Robot.cpp
+++ Freyja/src/Robot.cpp
@@ -26,15 +26,15 @@ void Robot::move(double Y, double X) {
 
 }
 void Robot::changeArmState(uint32_t button){
-	if(button ARM_FORWARD_BUTTON)){
+	if(button ARM_FORWARD_BUTTON){
 		robot -> setArmState(Arm::EXTENDING);
 	}
 	if(button ARM_REVERSE_BUTTON)){
 		robot -> setArmState(Arm::RETRACTING);
 	}
-	if(button ARM_FORWARD_BUTTON) && !button ARM_REVERSE_BUTTON))
+	if(button ARM_FORWARD_BUTTON) && !(button ARM_REVERSE_BUTTON)
 	{
 		robot -> setArmState(Arm::IDLE);
 	}
 }
-}
+
