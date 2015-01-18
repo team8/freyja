9dd956a33eed7af6c7e1e22100c2ef9f553f6623
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2015-01-18 12:51:16 -0800

    Added AutonomousController.cpp and AutonomousController.h

---
+++ Freyja/src/AutonomousController.cpp
@@ -0,0 +1,14 @@
+/*
+ * Version 1
+ * 1/18/15 at 1249
+ */
+
+#include <AutonomousController.h>
+
+AutonomousController::AutonomousController() {
+
+}
+
+AutonomousController::~AutonomousController() {
+}
+
---
+++ Freyja/src/AutonomousController.h
@@ -0,0 +1,15 @@
+/*
+ * Version 1
+ * 1/18/15 at 1249
+ */
+
+#ifndef SRC_AUTONOMOUSCONTROLLER_H_
+#define SRC_AUTONOMOUSCONTROLLER_H_
+
+class AutonomousController {
+public:
+	AutonomousController();
+	virtual ~AutonomousController();
+};
+
+#endif
