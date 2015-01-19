7cdace2fd65733dc55852f6b59f8eacde6276128
JulianKnodt <julianknodt@gmail.com>
2015-01-19 15:50:10 -0800

    Final minor fixes

--- Freyja/src/HumanController.cpp
+++ Freyja/src/HumanController.cpp
@@ -18,13 +18,13 @@ HumanController::HumanController() :
 void HumanController::update(Robot *robotPointer) {
 	robotPointer -> move(moveJoystick.GetY(),turnJoystick.GetX());
 
-	if(operatorJoystick.GetRawButton(1);){
+	if(operatorJoystick.GetRawButton(1));{
 		robotPointer -> changeArmState(Arm::State::EXTENDING)
 	}
-	if(operatorJoystick.GetRawButton(2);){
+	if(operatorJoystick.GetRawButton(2));{
 		robotPointer -> changeArmState(Arm::State::RETRACTING)
 	}
-	if(operatorJoystick.GetRawButton(3);){
+	if(operatorJoystick.GetRawButton(3));{
 		robotPointer -> changeArmState(Arm::State:IDLE)
 	}
 	if(operatorJoystick.GetRawButton(4);){
