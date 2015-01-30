cbae858518428c8ef83cd7d6163935517cd3207d
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2015-01-29 17:46:54 -0800

    changed auto to command based FSM from previous simple

--- Freyja/src/AutonomousController.h
+++ Freyja/src/AutonomousController.h
@@ -1,3 +1,9 @@
+/*
+ * Version 6
+ * 1/29/15
+ * Jonathan Zwiebel
+ */
+
 #ifndef AUTONOMOUS_CONTROLLER_H
 #define AUTONOMOUS_CONTROLLER_H
 
@@ -14,14 +20,55 @@ class AutonomousController {
 	public:
 		AutonomousController(Robot *robotPointer);
 		virtual ~AutonomousController();
-		int path;
 
 	private:
+		enum Path {
+			STOP = 0,
+			DRIVE = 1,
+			TOTE_SCORE = 2,
+			TOTE_SCORE_ACCUMULATE = 3,
+			TOTE_SCORE_DOUBLE_LEFT = 4,
+			TOTE_SCORE_DOUBLE_RIGHT = 5,
+			TOTE_SCORE_DOUBLE_LEFT_ACCUMULATE = 6,
+			TOTE_SCORE_DOUBLE_RIGHT_ACCUMULATE = 7,
+			TOTE_SCORE_TRIPLE = 8,
+			CAN_SCORE = 9,
+			CAN_SCORE_ACCUMULATE = 10,
+			ACCUMULATE = 11,
+			ACCUMULATE_DOUBLE = 12,
+			ACCUMULATE_TRIPLE = 13,
+		} path;
+
+
+		enum AutoCommand {
+			CMD_STOP = 0,
+			CMD_DRIVE = 1,
+			CMD_TOTE_SCORE = 2,
+			CMD_TOTE_SCORE_ACCUMULATE = 3,
+			CMD_TOTE_SCORE_DOUBLE_LEFT = 4,
+			CMD_TOTE_SCORE_DOUBLE_RIGHT = 5,
+			CMD_TOTE_SCORE_DOUBLE_LEFT_ACCUMULATE = 6,
+			CMD_TOTE_SCORE_DOUBLE_RIGHT_ACCUMULATE = 7,
+			CMD_TOTE_SCORE_TRIPLE = 8,
+			CMD_CAN_SCORE = 9,
+			CMD_CAN_SCORE_ACCUMULATE = 10,
+			CMD_ACCUMULATE = 11,
+			CMD_ACCUMULATE_DOUBLE = 12,
+			CMD_ACCUMULATE_TRIPLE = 13,
+			CMD_ACCUMULATE_FROM_SCORE = 14,
+			CMD_TOTE_TO_TOTE_RIGHT = 15,
+			CMD_TOTE_TO_TOTE_LEFT = 16,
+		} command;
+
 		Timer time;
 		Robot* robot;
 		AnalogInput dial;
 
-		//Various drive paths
+		void init();
+		void init(int p);
+		void update();
+		void callCommand(AutoCommand nc);
+
 		void stop();
 		void drive();
 		void toteScore();
@@ -38,6 +85,6 @@ class AutonomousController {
 		void accumulateTriple();
 		void accumulateFromScore();
 		void toteToTote(bool isRight);
-	};
+};
 
 #endif /* AUTONOMOUS_CONTROLLER_H */
