2a3c0da0873c1dda3c5c8ecd77b3046d5ca88e05
Eric Liu <ericxliu@gmail.com>
2015-02-01 16:02:19 -0800

    Added comments.

--- Freyja/src/Drivetrain.h
+++ Freyja/src/Drivetrain.h
@@ -8,34 +8,33 @@
 
 class Drivetrain: public Subsys {
 private:
+//Victors are speed controllers. We initialize them here.
 	Victor leftTopTalon;
-//	Victor leftMiddleTalon;
 	Victor leftBottomTalon;
 
 	Victor rightTopTalon;
-//	Victor rightMiddleTalon;
 	Victor rightBottomTalon;
 
 	double targetSpeed;
 	double rotateSpeed;
-
+//Encoder values are here. Encoders are things that measure the rotation of the wheels.
 	double leftEncoderVal;
 	double rightEncoderVal;
-
+//We initialize the Encoders here.
 	Encoder leftEncoder;
 	Encoder rightEncoder;
-
+//the gyro we use on the robot is here.
 	Gyro gyro;
+//PID controllers for the wheels. These essentially make sure the robot drives like it's supposed to.
 
 	PIDController leftTopController;
-//	PIDController leftMiddleController;
 	PIDController leftBottomController;
 
 	PIDController rightTopController;
-//	PIDController rightMiddleController;
 	PIDController rightBottomController;
 
 public:
+//making an enum called state that tells us what state the robot is in.
 	typedef enum State
 	{
 		IDLE,
@@ -45,10 +44,10 @@ public:
 	} State;
 
 	State state;
-
+//drivetrain constructor is here
 	Drivetrain();
 	virtual ~Drivetrain();
-
+//various methods
 	void init();
 
 	void disable();
