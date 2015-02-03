84eca883f5cb25966826ed3d29532a2db78fb9b8
JulianKnodt <julianknodt@gmail.com>
2015-02-02 18:24:10 -0800

    Added Comments!
    Engrish!

--- Freyja/src/Arm.h
+++ Freyja/src/Arm.h
@@ -3,6 +3,20 @@
 
 #include <WPILib.h>
 #include "Subsys.h"
+/*
+
+
+
+The arm is a subsytem of the robot(and is also kind of a subsystem of the whole lifter mechanism). It is essentially
+comprised of a solenoid, that controls the direction that the piston actuates, and a compressor, which gives the piston
+air to actuate. The code for them is simple, since it's all direction based, and the compressor outputs a given number of psi
+per second, thus, the code isn't too complex. The wrapper functions just check if the operatorJoystick button has been pressed,
+and changes an enum based on that.
+
+For more information, check the wiki, or consult the programmers referenced on the wiki.
+*/
+
+
 
 class Arm: public Subsys {
 public:
@@ -12,15 +26,17 @@ public:
 	enum PistonState {
 		EXTENDING, RETRACTING, IDLE, PUSH
 	};
-
+	//Constructor and Deconstructor
 	Arm();
 	virtual ~Arm();
+	//Enum changers which work with the update State Machine
 	void setPistonState(PistonState state);
 	void setCompressorState(CompressorState state);
+	//Inherited Methods from subsystem:
 	void init();
 	void update();
 	void disable();
-
+	//
 private:
 	Compressor compressor;
 	DoubleSolenoid solenoid;
