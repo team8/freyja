4cb5ddb03c4083fe35207b7e31e2d8b57c9c9e85
JulianKnodt <julianknodt@gmail.com>
2015-02-09 16:52:18 -0800

    Updated stubs

--- Freyja/src/Lights.cpp
+++ Freyja/src/Lights.cpp
@@ -16,32 +16,31 @@ void Lights::setLightState(LightState state){
         this->lightState = state;
 }
 void Lights::update(){
- switch(lightState){
- case ENABLED:
- for(int i = 0; i < 9; i ++){
-        digitalWrite(i, HIGH);
- }
- break;
- case DISABLED:
- for(int i = 9; i < 18; i ++){
-        digitalWrite(i, LOW);
- }
- break;
- case AUTONOMOUS:
- //Does something else undecided
- break;
- case LIGHTSOFF:
- for(int i = 0; i < 26; i ++){
-        digitalWrite(i, LOW);
- }
- break;
- default:
- setLightState(DISABLED);
- //When we flat out break
- break;
- }
-
-}
+        switch(lightState){
+                case ENABLED:
+                for(int i = PORT_ENABLED_START; i <= PORT_ENABLED_END; i ++){
+                        digitalWrite(i, HIGH);
+                }
+                break;
+                case DISABLED:
+                for(int i = PORT_DISABLED_START; i < PORT_DISABLED_END; i ++){
+                        digitalWrite(i, LOW);
+                }
+                break;
+                case AUTONOMOUS:
+                //Does something else undecided
+                break;
+                case LIGHTSOFF:
+                for(int i = 0; i < 26; i ++){
+                digitalWrite(i, LOW);
+                }
+                break;
+                default:
+                setLightState(DISABLED);
+                //When we flat out break
+                break;
+                }
+        }
 int Lights::main(){
         //Test Code
         digitalWrite(0, HIGH); 
