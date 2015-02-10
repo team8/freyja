112d58540be194a141a13bde4cd20f4b05a5696a
bencw99 <ben.cohenwang@gmail.com>
2015-02-09 20:49:49 -0800

    Commented Freyja class

--- Freyja/src/Freyja.cpp
+++ Freyja/src/Freyja.cpp
@@ -3,43 +3,67 @@
 #include "HumanController.h"
 #include "Robot.h"
 
+/**
+ * The class describing the robot and its controllers
+ * Implements WPI class of Iterative Robot, and appropriate functions
+ */
 class Freyja: public IterativeRobot {
 private:
+	/** The field representing the robot **/
 	Robot robot;
+	/** The field representing the human controller **/
 	HumanController humanController;
+	/** The field representing the autonomous controller **/
 	AutonomousController autoController;
 public:
+	/** The Freyja class constructor **/
 	Freyja();
 
+	/** Robot initialization, implemented from iterative robot **/
 	void RobotInit();
+	/** Disabled period initialization, implemented from iterative robot **/
 	void DisabledInit();
+	/** Disabled period update, implemented from iterative robot **/
 	void DisabledPeriodic();
+	/** Teleop period initialization, implemented from iterative robot **/
 	void TeleopInit();
+	/** Teleop period update, implemented from iterative robot **/
 	void TeleopPeriodic();
 };
 
+/** The Freyja class constructor **/
 Freyja::Freyja() :
+		//Constructor calls for robot, and controllers
 		robot(), humanController(&robot), autoController(&robot) {
 }
 
+/** Robot initialization, implemented from iterative robot **/
 void Freyja::RobotInit() {
+	//Initializes robot
 	robot.init();
 }
 
+/** Disabled period update, implemented from iterative robot **/
 void Freyja::DisabledInit() {
+	//Disables robot
 	robot.disable();
 }
 
+/** Disabled period update, implemented from iterative robot **/
 void Freyja::DisabledPeriodic() {
+	//Disables and updates robot
 	robot.disable();
 	robot.update();
 }
-
+/** Teleop period initialization, implemented from iterative robot **/
 void Freyja::TeleopInit() {
+	//Initializes robot
 	robot.init();
 }
 
+/** Teleop period update, implemented from iterative robot **/
 void Freyja::TeleopPeriodic() {
+	//Updates human controller and robot
 	humanController.update();
 	robot.update();
 }
