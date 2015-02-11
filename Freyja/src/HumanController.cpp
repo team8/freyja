12b06f71ba11c23b5c420d04e880c73e1fee3aa0
bencw99 <ben.cohenwang@gmail.com>
2015-02-11 12:51:58 -0800

    Commented human controller class

--- Freyja/src/HumanController.cpp
+++ Freyja/src/HumanController.cpp
@@ -3,13 +3,19 @@
 #include <stdio.h>
 using namespace std;
 
+/** Controller constructor, initializes fields */
 HumanController::HumanController(Robot *robot) :
+		//Initializes joysticks to appropriate ports
+		//Robot pointer initialized to parameter pointer
 		moveJoystick((uint32_t) PORT_SPEED), turnJoystick((uint32_t) PORT_TURN), operatorJoystick((uint32_t) PORT_OPERATOR), robotPointer(robot) {
 }
 
+/** Updates this human controller */
 void HumanController::update() {
+	//Sends movement command to robot drivetrain wrapper method based on joystick values
 	robotPointer->move(moveJoystick.GetY(), turnJoystick.GetX());
 
+	//Various button tests and resulting commands
 	if(operatorJoystick.GetRawButton(1)) {
 		robotPointer->changePistonState(Arm::PistonState::EXTENDING);
 	}
