b9b09fb7bb5e2992907c54bef8ea6375d361a513
bencw99 <ben.cohenwang@gmail.com>
2015-02-11 12:53:21 -0800

    Fixed freyja comment formatting

--- Freyja/src/Freyja.cpp
+++ Freyja/src/Freyja.cpp
@@ -16,54 +16,54 @@ private:
 	/** The field representing the autonomous controller **/
 	AutonomousController autoController;
 public:
-	/** The Freyja class constructor **/
+	/** The Freyja class constructor */
 	Freyja();
 
-	/** Robot initialization, implemented from iterative robot **/
+	/** Robot initialization, implemented from iterative robot */
 	void RobotInit();
-	/** Disabled period initialization, implemented from iterative robot **/
+	/** Disabled period initialization, implemented from iterative robot */
 	void DisabledInit();
-	/** Disabled period update, implemented from iterative robot **/
+	/** Disabled period update, implemented from iterative robot */
 	void DisabledPeriodic();
-	/** Teleop period initialization, implemented from iterative robot **/
+	/** Teleop period initialization, implemented from iterative robot */
 	void TeleopInit();
-	/** Teleop period update, implemented from iterative robot **/
+	/** Teleop period update, implemented from iterative robot */
 	void TeleopPeriodic();
 };
 
 using namespace std;
 
-/** The Freyja class constructor **/
+/** The Freyja class constructor */
 Freyja::Freyja() :
 		//Constructor calls for robot, and controllers
 		robot(), humanController(&robot), autoController(&robot) {
 }
 
-/** Robot initialization, implemented from iterative robot **/
+/** Robot initialization, implemented from iterative robot */
 void Freyja::RobotInit() {
 	//Initializes robot
 	robot.init();
 }
 
-/** Disabled period update, implemented from iterative robot **/
+/** Disabled period update, implemented from iterative robot */
 void Freyja::DisabledInit() {
 	//Disables robot
 	robot.disable();
 }
 
-/** Disabled period update, implemented from iterative robot **/
+/** Disabled period update, implemented from iterative robot */
 void Freyja::DisabledPeriodic() {
 	//Disables and updates robot
 	robot.disable();
 	robot.update();
 }
-/** Teleop period initialization, implemented from iterative robot **/
+/** Teleop period initialization, implemented from iterative robot */
 void Freyja::TeleopInit() {
 	//Initializes robot
 	robot.init();
 }
 
-/** Teleop period update, implemented from iterative robot **/
+/** Teleop period update, implemented from iterative robot */
 void Freyja::TeleopPeriodic() {
 	//Updates human controller and robot
 	humanController.update();
