19b0b23719afed4caca92d33f697f12142f7ba57
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2015-02-17 15:33:10 -0800

    added UDP Listener class

---
+++ Freyja/src/UDP_Listener.cpp
@@ -0,0 +1,79 @@
+#include "UDP_Listener.h"
+
+UDP_Listener::UDP_Listener(const std::string &port) :
+		port(port) {
+
+	tv.tv_sec = 0;
+	tv.tv_usec = 0;
+	FD_ZERO(&masterfds);
+	FD_ZERO(&readfds);
+
+	int status;
+	struct addrinfo hints, *servinfo, *p;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	if ((status = getaddrinfo(NULL, port.c_str(), &hints, &servinfo)) != 0) {
+		fprintf(stderr, "cRIO: getaddrinfo: %s\n", gai_strerror(status));
+	}
+
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol))
+				== -1) {
+			std::cerr << "cRIO: socket" << std::endl;
+			continue;
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			std::cerr << "cRIO: bind" << std::endl;
+			continue;
+		}
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "cRIO: failed to bind\n");
+	}
+	freeaddrinfo(servinfo);
+
+	FD_SET(sockfd, &masterfds);
+	FD_SET(sockfd, &readfds);
+	fdmax = sockfd;
+}
+
+UDP_Listener::~UDP_Listener() {
+	close(sockfd);
+}
+
+std::string UDP_Listener::recv() {
+
+	msg.clear();
+	char buf[BYTESMAX];
+	int bytesn;
+	readfds = masterfds;
+	if (select(fdmax + 1, &readfds, nullptr, nullptr, &tv) == -1) {
+		std::cerr << "select" << std::endl;
+		return "";
+	}
+	for (int i = 0; i <= fdmax; ++i) {
+		if (FD_ISSET(i, &readfds)) {
+			addr_len = sizeof(their_addr);
+
+			if ((bytesn = recvfrom(sockfd, buf, BYTESMAX - 1, 0,
+					(struct sockaddr *) &their_addr, &addr_len)) == -1) {
+				std::cerr << "recvfrom" << std::endl;
+				msg = "error";
+			} else {
+				buf[bytesn] = '\0';
+
+				msg.resize(bytesn);
+				msg = buf;
+			}
+		}
+	}
+	return msg;
+}
---
+++ Freyja/src/UDP_Listener.h
@@ -0,0 +1,54 @@
+
+#ifndef UDP_LISTENER_H
+#define UDP_LISTENER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#include <iostream>
+#include <string>
+
+/**
+  * @author Neelay Junnarkar
+  *
+  * The UDP_Listener class
+  *
+  * Status: Works, Jan. 31, 2015
+  */
+
+#define BYTESMAX 100
+
+class UDP_Listener {
+private:
+
+	const std::string port; //port the object listens on
+
+	int sockfd;
+	struct sockaddr_storage their_addr;
+	socklen_t addr_len;
+	std::string msg; //the message recieved
+
+	timeval tv;
+	fd_set masterfds, readfds;
+	int fdmax;
+
+public:
+	UDP_Listener(const std::string &port = "4950"); //port defaults to 4950
+	~UDP_Listener();
+
+	inline std::string getPort() const { return port; }
+
+	std::string recv(); //recovers from buffer and returns the string
+};
+
+#endif
